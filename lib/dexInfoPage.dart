fe5f14e04eab4492bb4ef028f635e0920e689616
Logan_Puatu <loganjpuatu@gmail.com>
2023-01-25 21:43:15 +0000

    fixed centering issue

--- lib/dexInfoPage.dart
+++ lib/dexInfoPage.dart
@@ -8,24 +8,25 @@ class PokeInfo extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        title: Text(detailPoke.dexName),
-        actions: [],
-        centerTitle: true,
-        elevation: 4,
-      ),
-      body: Column(children: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width * 0.3,
-          height: MediaQuery.of(context).size.height * 0.3,
-          child: Padding(
-            padding: EdgeInsetsDirectional.all(10),
-            child:
-                Image.asset('pokeSprites/homeSprite/' + detailPoke.homeSprite),
-          ),
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+          title: Text(detailPoke.dexName),
+          actions: [],
+          centerTitle: true,
+          elevation: 4,
         ),
-      ]),
-    );
+        body: Column(children: <Widget>[
+          Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.3,
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: Padding(
+                padding: EdgeInsetsDirectional.all(10),
+                child: Image.asset(
+                    'pokeSprites/homeSprite/' + detailPoke.homeSprite),
+              ),
+            ),
+          ),
+        ]));
   }
 }
