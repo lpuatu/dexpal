c407b1aa6d73436fee8faac692de0f5d94d09dc1
Logan_Puatu <loganjpuatu@gmail.com>
2023-01-26 00:58:09 +0000

    added search bar functionality, need to optimize

--- lib/dexViewPage.dart
+++ lib/dexViewPage.dart
@@ -2,6 +2,7 @@ import 'package:DexPal/dexInfoPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:csv/csv.dart';
+import 'package:easy_debounce/easy_debounce.dart';
 
 class pokeMon {
   String dexName;
@@ -22,18 +23,19 @@ class DexPage extends StatefulWidget {
 class DexPageState extends State<DexPage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   bool isLoading = false; // This is initially false where no loading state
-  String filterTerm = '';
-  List<pokeMon> dexTable = [];
-  TextEditingController? searchController;
+  List<pokeMon> fullDexTable = [];
+  List<pokeMon> viewDexTable = [];
+  var searchController = TextEditingController();
 
   void initState() {
     super.initState();
     dexLoad();
+    viewDexTable = fullDexTable;
     // On page load action.
   }
 
   void dispose() {
-    searchController?.dispose();
+    searchController.dispose();
     super.dispose();
   }
 
@@ -46,7 +48,7 @@ class DexPageState extends State<DexPage> {
     for (int i = 1;
         i < dexCsv.length;
         i++) //Start at row 1 becuase row 0 is the column names{
-      dexTable
+      fullDexTable
           .add(pokeMon(dexCsv[i][0], dexCsv[i][1], dexCsv[i][2], dexCsv[i][3]));
 
     setState(() {
@@ -54,11 +56,23 @@ class DexPageState extends State<DexPage> {
     });
   }
 
-  bool searchFunction(
-    String searchFor,
-    String searchIn,
-  ) {
-    return searchIn.toLowerCase().contains(searchFor.toLowerCase());
+  void searchFunction(String searchTerm) {
+    List<pokeMon> searchDexTable = [];
+    if (searchTerm.isEmpty) {
+      // if the search field is empty or only contains white-space, we'll display all users
+      searchDexTable = fullDexTable;
+    } else {
+      searchDexTable = fullDexTable
+          .where((poke) =>
+              poke.dexName.toLowerCase().contains(searchTerm.toLowerCase()))
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
+
+    // Refresh the UI
+    setState(() {
+      viewDexTable = searchDexTable;
+    });
   }
 
   @override
@@ -88,29 +102,38 @@ class DexPageState extends State<DexPage> {
           ),
         ]),
       ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height * 0.06,
-                        child: TextFormField(
-                          controller: searchController,
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
-                          obscureText: false,
-                          maxLines: 1,
-                        )),
-                  ],
-                ),
-                Row(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  child: TextFormField(
+                    controller: searchController,
+                    onChanged: (value) => EasyDebounce.debounce(
+                        'searchController', Duration(milliseconds: 2000),
+                        () async {
+                      searchFunction(value);
+                    }),
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      suffixIcon: IconButton(
+                        onPressed: searchController.clear,
+                        icon: Icon(Icons.clear),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    obscureText: false,
+                    maxLines: 1,
+                  )),
+            ],
+          ),
+          isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Container(
@@ -119,54 +142,51 @@ class DexPageState extends State<DexPage> {
                       child: ListView.builder(
                           physics: const AlwaysScrollableScrollPhysics(),
                           padding: const EdgeInsets.all(3),
-                          itemCount: dexTable.length,
+                          itemCount: viewDexTable.length,
                           itemBuilder: (BuildContext context, int index) {
-                            return Visibility(
-                              visible: true,
-                              child: Ink(
-                                color: Colors.white,
-                                child: InkWell(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(20)),
-                                  onTap: () async {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (BuildContext context) =>
-                                              PokeInfo(
-                                                  detailPoke: dexTable[index]),
-                                        ));
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      border: Border.all(
-                                        color: Colors.blue[50]!,
-                                      ),
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(20)),
+                            return Ink(
+                              color: Colors.white,
+                              child: InkWell(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                onTap: () async {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            PokeInfo(
+                                                detailPoke:
+                                                    viewDexTable[index]),
+                                      ));
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.blue[50]!,
                                     ),
-                                    height: MediaQuery.of(context).size.height *
-                                        0.08,
-                                    child: Row(children: [
-                                      Padding(
-                                          padding:
-                                              EdgeInsetsDirectional.fromSTEB(
-                                                  10, 0, 0, 0),
-                                          child: Image.asset(
-                                              'pokeSprites/teamSprite/' +
-                                                  dexTable[index].teamSprite)),
-                                      Spacer(),
-                                      Text(dexTable[index].dexName),
-                                      Spacer(),
-                                      Padding(
-                                          padding:
-                                              EdgeInsetsDirectional.fromSTEB(
-                                                  0, 0, 10, 0),
-                                          child: Text(dexTable[index]
-                                              .dexNum
-                                              .toString()))
-                                    ]),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20)),
                                   ),
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.08,
+                                  child: Row(children: [
+                                    Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(
+                                            10, 0, 0, 0),
+                                        child: Image.asset(
+                                            'pokeSprites/teamSprite/' +
+                                                viewDexTable[index]
+                                                    .teamSprite)),
+                                    Spacer(),
+                                    Text(viewDexTable[index].dexName),
+                                    Spacer(),
+                                    Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(
+                                            0, 0, 10, 0),
+                                        child: Text(viewDexTable[index]
+                                            .dexNum
+                                            .toString()))
+                                  ]),
                                 ),
                               ),
                             );
@@ -174,25 +194,25 @@ class DexPageState extends State<DexPage> {
                     ),
                   ],
                 ),
-                Container(
-                  color: Colors.red,
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  child: Row(
-                    children: [
-                      Spacer(),
-                      IconButton(
-                        splashRadius: 10,
-                        color: Colors.white,
-                        icon: const Icon(Icons.filter_alt),
-                        onPressed: () {
-                          scaffoldKey.currentState!.openEndDrawer();
-                        },
-                      )
-                    ],
-                  ),
-                ),
+          Container(
+            color: Colors.red,
+            height: MediaQuery.of(context).size.height * 0.05,
+            child: Row(
+              children: [
+                Spacer(),
+                IconButton(
+                  splashRadius: 10,
+                  color: Colors.white,
+                  icon: const Icon(Icons.filter_alt),
+                  onPressed: () {
+                    scaffoldKey.currentState!.openEndDrawer();
+                  },
+                )
               ],
             ),
+          ),
+        ],
+      ),
     );
   }
 }
