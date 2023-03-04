c32c11565ad9fc49ecb5de7df3c2f3316d15843c
Logan_Puatu <loganjpuatu@gmail.com>
2023-03-04 01:52:19 +0000

    weaknesses

--- lib/pokeWeakness.dart
+++ lib/pokeWeakness.dart
@@ -1,5 +1,7 @@
 import 'package:DexPal/pokeMon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:csv/csv.dart';
 
 class PokeWeak extends StatefulWidget {
   const PokeWeak({Key? key, required this.detailPoke}) : super(key: key);
@@ -16,7 +18,27 @@ class PokeWeakState extends State<PokeWeak> {
   @override
   void initState() {
     if (widget.detailPoke.type2 == 'Null') hastype2 = false;
+    getWeakness();
     super.initState();
+  }
+
+  void getWeakness() async {
+    final myData = await rootBundle.loadString("typeWeak.csv");
+    List<List<dynamic>> weakCsv = const CsvToListConverter().convert(myData);
+
+    int type1index = 1;
+    int type2index = 1;
+
+    Map<String, int> typeMap = {"null": 0};
+
+    for (int i = 1; i < weakCsv[0].length; i++) {
+      if (weakCsv[0][i] == widget.detailPoke.type1) i = type1index;
+      if (weakCsv[0][i] == widget.detailPoke.type2) i = type2index;
+    }
+
+    for (int j = 1; j < weakCsv.length; j++) {
+      typeMap[weakCsv[j][0]] = weakCsv[j][type1index] * weakCsv[j][type2index];
+    }
   }
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -38,8 +60,13 @@ class PokeWeakState extends State<PokeWeak> {
           elevation: 4,
         ),
       ),
+      body: Column(
+        children: [
+          Container(
+            child: Text(''),
+          ),
+        ],
+      ),
     );
   }
 }
-
-void getWeakness() {}
